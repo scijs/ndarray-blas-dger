92e8b5b7517f635649bd8c49af6dd2dee92cbfd7
Jalmari Raippalinna <jalmari@applifier.com>
2015-05-29 13:56:55 +0300

    reformat

--- test/test.js
+++ test/test.js
@@ -5,23 +5,23 @@ var assert = require('assert'),
   degr = require('../lib'),
   ndt = require('ndarray-tests');
 
-describe("degr()",function() {
+describe("degr()", function() {
 
   var A, x, y, x0, y0, alpha;
 
   beforeEach(function() {
-    A = ndarray([1,2,3,4,5,6], [2,3],[1,2]);
-    x = ndarray([1,2]);
-    x0 = ndarray([1,2]);
-    y = ndarray([3,4,5]);
-    y0 = ndarray([3,4,5]);
+    A = ndarray([1, 2, 3, 4, 5, 6], [2, 3], [1, 2]);
+    x = ndarray([1, 2]);
+    x0 = ndarray([1, 2]);
+    y = ndarray([3, 4, 5]);
+    y0 = ndarray([3, 4, 5]);
     alpha = -4;
   });
 
-  it('degr',function() {
-    assert( degr( alpha, x, y, A ) );
-    assert( ndt.approximatelyEqual( ndarray([3*alpha+1,6*alpha+2,4*alpha+3,8*alpha+4,5*alpha+5,10*alpha+6],[2,3],[1,2]), A, 1e-8 ), 'A contains the answer' );
-    assert( ndt.approximatelyEqual( y0, y, 1e-8 ), 'y is unchanged' );
-    assert( ndt.approximatelyEqual( x0, x, 1e-8 ), 'x is unchanged' );
+  it('degr', function() {
+    assert(degr(alpha, x, y, A));
+    assert(ndt.approximatelyEqual(ndarray([3 * alpha + 1, 6 * alpha + 2, 4 * alpha + 3, 8 * alpha + 4, 5 * alpha + 5, 10 * alpha + 6], [2, 3], [1, 2]), A, 1e-8), 'A contains the answer');
+    assert(ndt.approximatelyEqual(y0, y, 1e-8), 'y is unchanged');
+    assert(ndt.approximatelyEqual(x0, x, 1e-8), 'x is unchanged');
   });
 });
