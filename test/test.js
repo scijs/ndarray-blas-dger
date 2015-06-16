375eb16932b56af9e6d34c58fd538a6ddf7fcf79
Ricky Reusser <rsreusser@gmail.com>
2015-06-16 13:38:32 -0400

    Fix typo in test

--- test/test.js
+++ test/test.js
@@ -2,10 +2,10 @@
 
 var assert = require('assert'),
   ndarray = require('ndarray'),
-  degr = require('../lib'),
+  dger = require('../lib'),
   ndt = require('ndarray-tests');
 
-describe("degr()", function() {
+describe("dger()", function() {
 
   var A, x, y, x0, y0, alpha;
 
@@ -18,8 +18,8 @@ describe("degr()", function() {
     alpha = -4;
   });
 
-  it('degr', function() {
-    assert(degr(alpha, x, y, A));
+  it('dger', function() {
+    assert(dger(alpha, x, y, A));
     assert(ndt.approximatelyEqual(ndarray([3 * alpha + 1, 6 * alpha + 2, 4 * alpha + 3, 8 * alpha + 4, 5 * alpha + 5, 10 * alpha + 6], [2, 3], [1, 2]), A, 1e-8), 'A contains the answer');
     assert(ndt.approximatelyEqual(y0, y, 1e-8), 'y is unchanged');
     assert(ndt.approximatelyEqual(x0, x, 1e-8), 'x is unchanged');
